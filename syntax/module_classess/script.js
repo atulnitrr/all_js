8a1c7487a5017bfdc1fb55a9fd2546604684881c
Atul Kumar <atul.kumar3@go-mmt.com>
2020-04-28 08:18:40 +0530

    Added subclassingh

--- syntax/module_classess/script.js
+++ syntax/module_classess/script.js
@@ -1,17 +1,82 @@
 /*
+class myArray extends Array {
+  convert() {
+    let returnedArray = [];
+    this.forEach((value) => returnedArray.push("Converted " + value));
+    return returnedArray;
+  }
+}
+
+const muar = new myArray();
+muar.push(2);
+muar.push(3);
+muar.push(4);
+console.log(muar.convert());
+
+*/
+
+/*
+class Person {
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name.toUpperCase();
+  }
+
+  set name(value) {
+    if (value.length <= 2) {
+      console.log("Rejected");
+    }
+    this._name = value;
+  }
+}
+
+let person = new Person("atul");
+console.log(person);
+console.log(person.name);
+person.name = "akkk";
+console.log(person);
+console.log(person.name);
+*/
+/*
+class Helper {
+  static logTwice(message) {
+    console.log(message);
+    console.log(message);
+  }
+}
+
+Helper.logTwice("darta");
+
 class Person {
   constructor(name) {
     this.name = name;
   }
   greet() {
-    console.log(`Heelo my name is ${this.name}`);
+    console.log(`Heelo my name is ${this.name} age : ${this.age}`);
   }
 }
+
+class Atul extends Person {
+  constructor(age) {
+    super("Atul");
+    this.age = age;
+  }
+}
+
+let ak = new Atul(23);
+console.log(ak);
+console.log(ak.__proto__);
+ak.greet();
 
 let person = new Person("KKKK");
 console.log(person);
 person.greet();
+
 */
+
 /*
 function Per() {}
 let per = new Per();
