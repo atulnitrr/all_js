26d9dc0c3fc6fb90c860e36896b7150f6dea815a
Atul Kumar <atul.kumar3@go-mmt.com>
2020-04-28 07:41:46 +0530

    Added module

---
+++ syntax/module_classess/external.js
@@ -0,0 +1,8 @@
+let keyValue = 10;
+function test() {
+  console.log("test data");
+}
+
+// export default test;
+
+export { keyValue, test };
---
+++ syntax/module_classess/script.js
@@ -0,0 +1,9 @@
+// import { keyValue } from "./external.js";
+// import test from "./external.js";
+
+import * as allImp from "./external.js";
+
+console.log(allImp);
+console.log(allImp.keyValue);
+// console.log(keyValue);
+// test();
